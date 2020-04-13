050e2b99ba4d27aae5de138c6bc92cb3ebce31f0
Srilaya Bhavaraju <srilaya.bhavaraju08@gmail.com>
2020-04-13 13:32:52 -0400

    Changed cell representation to be single element

--- maze.py
+++ maze.py
@@ -3,14 +3,12 @@
 import random
 
 '''
-TODO: -specify goal
-	  -number of walls
-	  -better vis
+TODO: -hardcode connected walls for some cases
 '''
 
 class Cell:
 	'''
-	Represents a single cell in a maze.
+	Represents a single cell in a maze, each with four walls.
 	'''
 
 	def __init__(self, row, col):
@@ -36,6 +34,7 @@ class Cell:
 		other_cell.walls[other_wall] = False
 
 
+
 class Maze:
 	'''Represents a maze as a grid of cells'''
 
@@ -50,6 +49,9 @@ class Maze:
 
 		self.maze = [[Cell(row, col) for col in range(num_cols)] for row in range(num_rows)]
 
+		self.final_maze = None
+
+
 
 	def make_maze(self):
 		'''Creates maze that is solvable'''
@@ -60,6 +62,8 @@ class Maze:
 		current_cell = self.get_cell(self.start_row, self.start_col)
 
 		current_cell_number = 1
+
+		self.set_goal()
 
 		while current_cell_number < total_cells:
 			neighbors = self.get_neighbors(current_cell)
@@ -74,8 +78,6 @@ class Maze:
 			stack.append(current_cell)
 			current_cell = next_cell
 			current_cell_number += 1
-			# print (self.num_interior_walls)
-			# print (self)
 
 
 
@@ -114,9 +116,6 @@ class Maze:
 			self.num_interior_walls -= 1
 			stack.append(current_cell)
 			current_cell = next_cell
-			# print (self.num_interior_walls)
-			# print (self)
-
 
 
 	def get_all_neighbors(self, cell):
@@ -185,13 +184,14 @@ class Maze:
 
 
 	def generate_maze(self):
-		'''Generates overall maze with specified number of walls and random goal position'''
+		'''Generates overall maze of maze_cells with specified number of walls and random goal position'''
 		self.make_maze_with_num_walls()
 		self.set_goal()
+		self.generate_maze_of_maze_cells()
 
 
-	def __str__(self):
-		'''Returns string representation of maze'''
+	def print_preliminary_maze(self):
+		'''Returns string representation of preliminary maze'''
 		maze = ' '
 		for i in range(self.num_cols):
 			maze += '__ '
@@ -234,10 +234,128 @@ class Maze:
 
 		return maze
 
+	def generate_maze_of_maze_cells(self):
 
 
-m = Maze(3, 3, 4, 0, 0)
+		num_rows = self.num_rows*2 + 1
+		num_cols = self.num_cols*2 + 1
+
+		new_maze = [[Maze_Cell(row, col) for col in range(num_cols)] for row in range(num_rows)]
+
+		for row in range(self.num_rows):
+			for col in range(self.num_cols):
+				cell = self.get_cell(row, col)
+				row = cell.row
+				col = cell.col
+				walls = cell.walls
+
+				new_row = 2*row + 1
+				new_col = 2*col + 1
+
+				if row == self.start_row and col == self.start_col:
+					new_maze[new_row][new_col].set_start()
+				elif row == self.goal.row and col == self.goal.col:
+					new_maze[new_row][new_col].set_goal()
+
+
+				north = (new_row-1, new_col)
+				south = (new_row+1, new_col)
+				east = (new_row, new_col+1)
+				west = (new_row, new_col-1)
+
+				if walls['N'] and (0<=north[0]<num_rows and 0<=north[1]<num_cols):
+					new_maze[north[0]][north[1]].set_wall()
+
+				if walls['S'] and (0<=south[0]<num_rows and 0<=south[1]<num_cols):
+					new_maze[south[0]][south[1]].set_wall()
+
+				if walls['E'] and (0<=east[0]<num_rows and 0<=east[1]<num_cols):
+					new_maze[east[0]][east[1]].set_wall()
+
+				if walls['W'] and (0<=west[0]<num_rows and 0<=west[1]<num_cols):
+					new_maze[west[0]][west[1]].set_wall()
+
+
+			for col in range(num_cols):
+				new_maze[0][col].set_wall()
+				new_maze[num_rows-1][col].set_wall()
+
+			for row in range(num_rows):
+				new_maze[row][0].set_wall()
+				new_maze[num_cols-1][col].set_wall()
+
+			self.final_maze = new_maze
+
+	def __str__(self):
+
+		num_rows = 2*self.num_rows+1
+		num_cols = 2*self.num_cols+1
+
+		new_maze = [[0 for col in range(num_cols)] for row in range(num_rows)]
+
+		for new_row in range(num_rows):
+			for new_col in range(num_cols):
+				cell = self.final_maze[new_row][new_col]
+				row = cell.row
+				col = cell.col
+
+				new_maze[new_row][new_col] = str(cell)
+
+		str_new_maze = ''
+		for row in new_maze:
+			str_new_maze += str(row)
+			str_new_maze += '\n'
+
+		return str_new_maze
+
+
+
+
+class Maze_Cell:
+	'''
+	Represents a single cell in a maze, treating walls as separate cells
+	'''
+
+	def __init__(self, row, col):
+		self.row = row
+		self.col = col
+		self.is_wall = False
+		self.is_start = False
+		self.is_goal = False
+
+	def is_wall(self):
+		return self.is_wall
+
+	def is_start(self):
+		return self.is_start
+
+	def is_goal(self):
+		return self.is_goal
+
+	def set_goal(self):
+		self.is_goal = True
+
+	def set_wall(self):
+		self.is_wall = True
+
+	def set_start(self):
+		self.is_start = True
+
+
+	def __str__(self):
+		if self.is_goal:
+			return "G"
+		if self.is_wall:
+			return "W"
+		return " "
+
+
+
+
+m = Maze(3, 3, 2, 0, 0)
 m.generate_maze()
+prelim = m.print_preliminary_maze()
+print (prelim)
 print (m)
 
 
